122d9e3019da92799ec4186d29d766bccac58d2b
Nathnael Yirga <Nathnaelyirga@gmail.com>
2024-10-27 17:34:28 +0300

    minor touches

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import secrets
 
-from flask import redirect, render_template, request, url_for
+from flask import redirect, render_template, url_for
 
 from app import app, db
 from app.forms import ChatForm, JoinRoom, NewRoom
@@ -10,54 +10,69 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
+    roomJoinForm = JoinRoom()
+    createRoomForm = NewRoom()
 
-	roomJoinForm = JoinRoom()
-	createRoomForm = NewRoom()
+    if createRoomForm.newSubmit.data and createRoomForm.validate_on_submit():
+        key = secrets.token_hex(8)
+        room = Room(key=key)
 
-	if createRoomForm.newSubmit.data and createRoomForm.validate_on_submit():
-		
-		key=secrets.token_hex(8)
-		room = Room(key=key)
+        db.session.add(room)
+        db.session.commit()
+        print(f"room {key} created!")
 
-		db.session.add(room)
-		db.session.commit()
+        return render_template(
+            "home.html",
+            roomJoinForm=roomJoinForm,
+            createRoomForm=createRoomForm,
+            title="Chat-App:Home",
+            message=key,
+        )
 
-		return render_template("home.html", roomJoinForm = roomJoinForm, createRoomForm = createRoomForm, title="Chat-App:Home", message=key)
+    if roomJoinForm.joinSubmit.data and roomJoinForm.validate_on_submit():
+        key = roomJoinForm.key.data
 
-	if roomJoinForm.joinSubmit.data and roomJoinForm.validate_on_submit():
+        if key:
+            room = Room.query.filter_by(key=key).first()
+            if room:
+                room_id = room.id
+                return redirect(url_for("room", id=room_id))
+            else:
+                print("Room ID not found!")
+        else:
+            print("Key not found!")
+    # return render_template("room.html", form=ChatForm(), message=room_id)
+    return render_template(
+        "home.html",
+        roomJoinForm=roomJoinForm,
+        createRoomForm=createRoomForm,
+        title="Chat-App:Home",
+    )
 
-		key = roomJoinForm.key.data
-		room_id = Room.query.filter_by(key=key).first()
-		if room_id:
-			room_id = room_id.id
 
-			return redirect(url_for('room', id = room_id))
-		# return render_template("room.html", form=ChatForm(), message=room_id)
-
-	return render_template("home.html", roomJoinForm = roomJoinForm, createRoomForm = createRoomForm, title="Chat-App:Home")
-
-
-@app.route("/room/<id>", methods=['GET', 'POST'])
+@app.route("/room/<id>", methods=["GET", "POST"])
 def room(id):
+    chatForm = ChatForm()
+    if chatForm.messageSubmit.data:
+        print("MSG")
 
-	chatForm = ChatForm()
-	print(1)
-	if chatForm.messageSubmit.data:
-		print("MSG")
+        content = chatForm.textMessage.data
+        _message = Messages(content=content, room_id=id)
 
-		content = chatForm.textMessage.data
-		_message = Messages(content = content, room_id = id)
-		
-		db.session.add(_message)
-		db.session.commit()
+        db.session.add(_message)
+        db.session.commit()
 
-		chatForm.textMessage.data = ""
+        chatForm.textMessage.data = ""
 
-	messages = Room.query.filter_by(id=id).first().messages
-	message_count = len(messages)
+    messages = Room.query.filter_by(id=id).first().messages
+    message_count = len(messages)
 
-	return render_template("room.html", form=chatForm, title="Chat-App:Room", messages = messages, message_count =message_count)
-
-
+    return render_template(
+        "room.html",
+        form=chatForm,
+        title="Chat-App:Room",
+        messages=messages,
+        message_count=message_count,
+    )
