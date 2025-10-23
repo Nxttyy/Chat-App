13944b2a02bdbc110827511dadc88b18a2f2470d
Nathnael <nathnaelyirga@gmail.com>
2025-10-23 22:10:07 +0300

    [chore]: vercel, uv ...

--- app/extensions.py
+++ app/extensions.py
@@ -8,12 +8,13 @@ socketio = SocketIO()
 @socketio.on("my event")
 def handle_message(data):
     # print(data)
-    print("received message: " + data["data"])
+    # print("received message: " + data["data"])
+    return
 
 
 # Socket.IO event for joining a room
 @socketio.on("join")
 def on_join(data):
-    print("new join")
+    # print("new join")
     room_id = data["room_id"]
     join_room(room_id)
